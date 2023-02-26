8999d2ab929f179160c426b96d2a1269a6ee8996
Bert Outtier <outtierbert@gmail.com>
2023-02-26 10:21:47 +0100

    fix setup platforms for HA 2023.03

--- custom_components/ilexconnect/__init__.py
+++ custom_components/ilexconnect/__init__.py
@@ -32,7 +32,7 @@ async def async_setup_entry(hass, config_entry):
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][config_entry.entry_id] = coordinator
 
-    hass.config_entries.async_setup_platforms(config_entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
 
     return True
 
